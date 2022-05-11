29e796d680320cad8005173f7c5c651fd4fa28f2
Johnny-Oleg <neojin.entity@gmail.com>
2022-05-11 20:45:27 +0300

    main page responsive complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 const $faqIcon = document.querySelectorAll('.faq__container-icon');
 const $mobileIcon = document.querySelector('.icon');
+const $navList = document.querySelector('.header__menu-list');
 const $iconArr = [...$faqIcon];
 
 $iconArr.forEach(item => {
 	item.addEventListener('click', () => item.parentNode.classList.toggle('active'));
 })
 
-$mobileIcon.addEventListener('click', () => $mobileIcon.classList.toggle('open'));
+$mobileIcon.addEventListener('click', () => {
+	$mobileIcon.classList.toggle('open');
+	$navList.classList.toggle('active');
+});
